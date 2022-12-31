bc9fd554edf4f0270bb55544a91d8fec107a2e95
Joelayo <joetech.ooj@gmail.com>
2022-12-31 10:18:55 +0100

    Task 7 Complete

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,26 +4,7 @@ export default class Airport {
     this._code = code;
   }
 
-  // Getters for the name and code attributes
-  get name() {
-    return this._name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  // Setters for the name and code attributes
-  set name(value) {
-    this._name = value;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
-  // Default string description of the class
   toString() {
-    return this.code;
+    return this._code;
   }
 }
