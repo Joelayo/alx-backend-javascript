ce469630bc3a7e720ed1215294c16e6768022d56
Joelayo <joetech.ooj@gmail.com>
2022-12-31 10:40:26 +0100

    Task 5 Complete

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,19 +3,16 @@ export default class Building {
     this._sqft = sqft;
   }
 
-  // Getter for the sqft attribute
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for the sqft attribute
-  set sqft(value) {
-    this._sqft = value;
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 
-  // Abstract method that should be implemented by classes that extend from Building
-  evacuationWarningMessage(message = 'Class extending Building must override evacuationWarningMessage') {
-    this._message = message;
-    return this._message;
+  /*eslint-disable*/
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
